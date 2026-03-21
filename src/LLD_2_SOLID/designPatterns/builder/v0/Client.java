dd7708df76a9ef373df568a3eb03e2a8e8af26c2
Stalin2391 <stalind3@gmail.com>
2026-03-21 23:08:35 +0530

    builder validation

--- src/LLD_2_SOLID/designPatterns/builder/v0/Client.java
+++ src/LLD_2_SOLID/designPatterns/builder/v0/Client.java
@@ -4,8 +4,9 @@ public class Client {
     public static void main(String[] args) {
         Builder builder = new Builder();
         builder.setName("John Doe");
+        builder.setAge(20);
         builder.setRollNo("12345");
         Student student = new Student(builder);
-        System.out.println(student);
+        System.out.println(student.age);
     }
 }
--- src/LLD_2_SOLID/designPatterns/builder/v0/Student.java
+++ src/LLD_2_SOLID/designPatterns/builder/v0/Student.java
@@ -9,6 +9,12 @@ public class Student {
    String batch;
 
    Student(Builder builder) {
+
+       if(builder.name == null || builder.rollNo == null) {
+           throw new IllegalArgumentException("Name and Roll No cannot be null");
+       } else if(builder.age < 0) {
+           throw new IllegalArgumentException("Age cannot be negative");
+       }
        this.name = builder.name;
        this.rollNo = builder.rollNo;
        this.age = builder.age;
